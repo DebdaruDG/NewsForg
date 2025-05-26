b2a6070410a0d94e063d3c012b386de7c1694f7c
Debdaru Dasgupta <debdaru.dasgupta@tci.email>
2025-05-26 17:16:47 +0530

    updated

--- B2B - BE Server/src/index.ts
+++ B2B - BE Server/src/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import { config } from './config/env';
 import { errorHandler } from './middleware/error.middleware';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
-app.use(cors({ origin: 'https://your-frontend.com' })); // Adjust origin as needed
+app.use(cors({ origin: '*' })); // Adjust origin as needed
 app.use(express.json());
 
+const swaggerDocument = require('../swagger.json');
 // API routes
 app.use('/api/v1/auth', require('./api/v1/auth'));
 app.use('/api/v1/companies', require('./api/v1/companies'));
@@ -15,8 +17,6 @@ app.use('/api/v1/articles', require('./api/v1/articles'));
 app.use('/api/v1/roles', require('./api/v1/roles'));
 
 // Swagger API documentation
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json';
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
